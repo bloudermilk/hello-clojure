4d38da24b462565bd1f3294af862342e9f2c453c
Brendan Loudermilk <brendan@gophilosophie.com>
2012-04-24 22:02:53 -0700

    Small updates

--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -1,12 +1,11 @@
 (ns hello-clojure.core
   (:gen-class)
-  (:use ring.adapter.jetty))
+  (:use [ring.adapter.jetty :only [run-jetty]]))
 
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body "Hello, Clojure!"})
 
-(defn -main []
-  (let [port (Integer/parseInt (System/getenv "PORT"))]
-    (run-jetty app {:port port})))
+(defn -main [port]
+  (run-jetty app {:port (Integer. port)}))
