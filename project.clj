62576d715e0b85bb9d087df4820297d139821bd0
Brendan Loudermilk <brendan@gophilosophie.com>
2012-04-24 21:23:43 -0700

    Updated ring-jetty-adapter dep

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hello-clojure "1.0.0-SNAPSHOT"
   :description "My first clojure app!"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring/ring-jetty-adapter "0.3.9"]]
+                 [ring/ring-jetty-adapter "1.1.0"]]
   :main hello-clojure.core)
